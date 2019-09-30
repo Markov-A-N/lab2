7837b251c8034231fd19d406d91957b4bef22eb5
Markov-A-N <markov.lifeacc@gmail.com>
2019-09-30 15:19:14 +0300

    m

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 
 	std::cout << "ob1 - ob2\n" << ob1 - ob2;
 
-	double n{};
+	double n;
 	std::cin >> n;
 	std::cout << "ob1 * " << std::fixed << n << ":\n" << ob1 * n;
 	std::cin >> n;
